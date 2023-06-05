84f1dae1e63f21d795e399f077642db81c2a7663
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2023-06-05 09:12:37 -0300

    adding tag owner

--- iac/app.py
+++ iac/app.py
@@ -36,7 +36,8 @@ else:
 tags = {
     'project': 'DevMedias',
     'stage': stage,
-    'stack': 'BACK'
+    'stack': 'BACK',
+    'owner': 'DevCommunity',
 }
 
 IacStack(app, stack_name, env=cdk.Environment(account=aws_account_id, region=aws_region), tags=tags)
