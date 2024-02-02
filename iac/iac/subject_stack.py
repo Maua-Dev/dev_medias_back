55a6ae73fbebe30f4b57ef8dd307d93f542bff98
Vitor Soller <vgsoller@gmail.com>
2024-02-02 14:47:16 -0300

    change oac

--- iac/iac/subject_stack.py
+++ iac/iac/subject_stack.py
@@ -36,27 +36,20 @@ class SubjectStack(Construct):
             "signingProtocol": "sigv4"
         })
 
-        aws_cloudfront.CfnDistribution(self, "CDN", distribution_config={
-            "comment": f"DevMedias Subject Bucket CDN {self.github_ref}",
-            "origins": [
-                {
-                    "domainName": self.bucket.bucket_domain_name,
-                    "id": "s3-origin",
-                    "originPath": "",
-                    "s3OriginConfig": {
-                        "originAccessIdentity": f"origin-access-identity/cloudfront/{oac.ref}"
-                    }
-                }
-            ],
-            "defaultCacheBehavior": {
-                "targetOriginId": "s3-origin",
-                "viewerProtocolPolicy": "redirect-to-https",
-                "allowedMethods": ["GET", "HEAD"],
-                "cachedMethods": ["GET", "HEAD"],
-                "compress": True,
-                "minTtl": 0,
-                "maxTtl": 86400,
-                "defaultTtl": 3600
-            },
-            "enabled": True
-        })
+        cloudFrontWebDistribution = aws_cloudfront.CloudFrontWebDistribution(self, "CloudFrontWebDistribution",
+                                                                             origin_configs=[
+                                                                                 aws_cloudfront.SourceConfiguration(
+                                                                                     s3_origin_source=aws_cloudfront.S3OriginConfig(
+                                                                                         s3_bucket_source=self.bucket,
+                                                                                     ),
+                                                                                     behaviors=[aws_cloudfront.Behavior(
+                                                                                         is_default_behavior=True)]
+                                                                                 )
+
+                                                                             ],
+                                                                             price_class=aws_cloudfront.PriceClass.PRICE_CLASS_ALL,
+                                                                             viewer_protocol_policy=aws_cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                                                                             )
+
+        cfn_distribution = cloudFrontWebDistribution.node.default_child
+        cfn_distribution.add_property_override('DistributionConfig.Origins.0.OriginAccessIdentity', oac.get_att('Id'))
