7108ecb1739aaefbe9caff4594e7b66007872429
Joao Branco <21.01075-7@maua.br>
2024-07-02 19:56:30 -0300

    refat(Solucionador): remove print

--- src/shared/solucionador.py
+++ src/shared/solucionador.py
@@ -22,7 +22,6 @@ class Solucionador:
 
         # lista que conterá as notas possíveis de serem retornadas
         notas_possiveis = list()
-        print(f"Tamanho de notas_possiveis variable: {len(notas_possiveis)}")
 
         # Se não for possível atingir tal nota, retornará uma lista vazia
         # ex: se o aluno escolher média 10, e tirou 0 em alguma nota, esse "if" captará
