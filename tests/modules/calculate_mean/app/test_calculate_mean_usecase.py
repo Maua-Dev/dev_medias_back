bcb20151fc09690471a1986529af42dcf3b1d581
Vinícius de Oliveira Berti <vinicius.berti33@gmail.com>
2025-01-16 19:52:28 -0300

    fix: usecase test

--- tests/modules/calculate_mean/app/test_calculate_mean_usecase.py
+++ tests/modules/calculate_mean/app/test_calculate_mean_usecase.py
@@ -12,20 +12,22 @@ from src.shared.solucionador import Solucionador
 class TestCalculateMeanUsecase:
 
     def test_calculate_mean_usecase(self):
-        P1 = Nota(peso=0.6*0.4, valor=5)
-        P2 = Nota(peso=0.6*0.6, valor=4)
+    
+        P1 = Nota(peso=0.4, valor=5)
+        P2 = Nota(peso=0.6, valor=4)
         provas_que_tenho = [P1, P2]
-        
-        T1 = Nota(peso=0.4*0.4, valor=9.5)
-        T2 = Nota(peso=0.4*0.6, valor=7)
+
+        T1 = Nota(peso=0.5, valor=9.5)
+        T2 = Nota(peso=0.5, valor=7)
         trabalhos_que_tenho = [T1, T2]
-        
-        
+
+        peso_prova = 0.6
+        peso_trabalho = 0.4
 
         usecase = CalculateMeanUsecase()
-        mean = usecase(provas_que_tenho=provas_que_tenho, trabalhos_que_tenho=trabalhos_que_tenho)
+        mean = usecase(peso_prova=peso_prova, peso_trabalho=peso_trabalho, provas_que_tenho=provas_que_tenho, trabalhos_que_tenho=trabalhos_que_tenho)
 
-        assert mean == 5.8
+        assert mean == 6.0
 
     def test_calculate_mean_empty_list(self):
         provas_que_tenho = []
