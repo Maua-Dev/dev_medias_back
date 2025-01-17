9291793e95e90d601a83bf31bdcfcc244e253fbd
LucasCrapino <163664488+LucasCrapino@users.noreply.github.com>
2025-01-17 12:54:01 -0300

    fix: grade_optmizer viewmodel tests

--- tests/modules/grade_optimizer/app/test_grade_optmizer_viewmodel.py
+++ tests/modules/grade_optimizer/app/test_grade_optmizer_viewmodel.py
@@ -8,19 +8,22 @@ from src.shared.domain.entities.nota import Nota
 class TestGradeOptimizerViewmodel:
 
     def test_grade_optmizer_viewmodel(self):
-        P1 = Nota(peso=0.24, valor=1.0)
+        P1 = Nota(peso=0.4, valor=1.0)
         provas_que_tenho = [P1]
         
         trabalhos_que_tenho = []
         
-        P2 = Nota(peso=0.36, valor=None)
+        P2 = Nota(peso=0.6, valor=None)
         provas_que_quero = [P2]
         
-        T1 = Nota(peso=0.12, valor=None)
-        T2 = Nota(peso=0.28, valor=None)
+        T1 = Nota(peso=0.4, valor=None)
+        T2 = Nota(peso=0.6, valor=None)
         trabalhos_que_quero = [T1, T2]
         
-        boletim = Boletim(provas_que_quero=provas_que_quero, provas_que_tenho=provas_que_tenho, trabalhos_que_quero=trabalhos_que_quero, trabalhos_que_tenho=trabalhos_que_tenho)
+        peso_prova = P1.peso + P2.peso
+        peso_trabalho = T1.peso + T2.peso
+
+        boletim = Boletim(peso_prova=peso_prova, peso_trabalho=peso_trabalho, provas_que_quero=provas_que_quero, provas_que_tenho=provas_que_tenho, trabalhos_que_quero=trabalhos_que_quero, trabalhos_que_tenho=trabalhos_que_tenho)
         
         boletim.quero[0].valor = 7.5
         boletim.quero[1].valor = 7.5
@@ -34,17 +37,17 @@ class TestGradeOptimizerViewmodel:
                 "provas":[
                     {
                         "valor":7.5,
-                        "peso":0.36
+                        "peso":0.6
                     }
                 ],
                 "trabalhos":[
                     {
                         "valor":7.5,
-                        "peso":0.12
+                        "peso":0.4
                     },
                     {
                         "valor":8,
-                        "peso":0.28
+                        "peso":0.6
                     }
                 ]
             },
@@ -52,19 +55,20 @@ class TestGradeOptimizerViewmodel:
         }
         
     def test_grade_optmizer_viewmodel_grades_somente_provas(self):
-        P1 = Nota(peso=0.24, valor=1.0)
+        P1 = Nota(peso=0.2, valor=1.0)
         provas_que_tenho = [P1]
         trabalhos_que_tenho = []
         
-        
-        P2 = Nota(peso=0.36, valor=None)
-        P3 = Nota(peso=0.28, valor=None)
-        P4 = Nota(peso=0.12, valor=None)
+        P2 = Nota(peso=0.2, valor=None)
+        P3 = Nota(peso=0.3, valor=None)
+        P4 = Nota(peso=0.3, valor=None)
         provas_que_quero = [P2, P3, P4]
         trabalhos_que_quero = []
         
-        
-        boletim = Boletim(provas_que_quero=provas_que_quero, provas_que_tenho=provas_que_tenho, trabalhos_que_quero=trabalhos_que_quero, trabalhos_que_tenho=trabalhos_que_tenho)
+        peso_prova = P1.peso + P2.peso + P3.peso + P4.peso
+        peso_trabalho = 1.0
+
+        boletim = Boletim(peso_prova=peso_prova, peso_trabalho=peso_trabalho, provas_que_quero=provas_que_quero, provas_que_tenho=provas_que_tenho, trabalhos_que_quero=trabalhos_que_quero, trabalhos_que_tenho=trabalhos_que_tenho)
         
         boletim.quero[0].valor = 7.5
         boletim.quero[1].valor = 7.5
@@ -78,15 +82,15 @@ class TestGradeOptimizerViewmodel:
                 "provas":[
                     {
                         "valor":7.5,
-                        "peso":0.36
+                        "peso":0.2
                     },
                     {
                         "valor":7.5,
-                        "peso":0.28
+                        "peso":0.3
                     },
                     {
                         "valor":8,
-                        "peso":0.12
+                        "peso":0.3
                     }
                 ],
                 "trabalhos":[
@@ -97,19 +101,21 @@ class TestGradeOptimizerViewmodel:
         }
         
     def test_grade_optmizer_viewmodel_grades_somente_trabalhos(self):
-        T1 = Nota(peso=0.24, valor=1.0)
+        T1 = Nota(peso=0.2, valor=1.0)
         provas_que_tenho = []
         trabalhos_que_tenho = [T1]
         
         
-        T2 = Nota(peso=0.36, valor=None)
-        T3 = Nota(peso=0.28, valor=None)
-        T4 = Nota(peso=0.12, valor=None)
+        T2 = Nota(peso=0.2, valor=None)
+        T3 = Nota(peso=0.3, valor=None)
+        T4 = Nota(peso=0.3, valor=None)
         provas_que_quero = []
         trabalhos_que_quero = [T2, T3, T4]
         
-        
-        boletim = Boletim(provas_que_quero=provas_que_quero, provas_que_tenho=provas_que_tenho, trabalhos_que_quero=trabalhos_que_quero, trabalhos_que_tenho=trabalhos_que_tenho)
+        peso_prova = 1.0
+        peso_trabalho = T1.peso + T2.peso + T3.peso + T4.peso
+
+        boletim = Boletim(peso_prova=peso_prova, peso_trabalho=peso_trabalho, provas_que_quero=provas_que_quero, provas_que_tenho=provas_que_tenho, trabalhos_que_quero=trabalhos_que_quero, trabalhos_que_tenho=trabalhos_que_tenho)
         
         boletim.quero[0].valor = 7.5
         boletim.quero[1].valor = 7.5
@@ -125,15 +131,15 @@ class TestGradeOptimizerViewmodel:
                 "trabalhos":[
                     {
                         "valor":7.5,
-                        "peso":0.36
+                        "peso":0.2
                     },
                     {
                         "valor":7.5,
-                        "peso":0.28
+                        "peso":0.3
                     },
                     {
                         "valor":8,
-                        "peso":0.12
+                        "peso":0.3
                     }
                 ]
             },
@@ -141,19 +147,21 @@ class TestGradeOptimizerViewmodel:
         }
         
     def test_grade_optmizer_viewmodel_grades_not_found(self):
-        T1 = Nota(peso=0.24, valor=1.0)
+        T1 = Nota(peso=0.2, valor=1.0)
         provas_que_tenho = []
         trabalhos_que_tenho = [T1]
         
         
-        T2 = Nota(peso=0.36, valor=None)
-        T3 = Nota(peso=0.28, valor=None)
-        T4 = Nota(peso=0.12, valor=None)
+        T2 = Nota(peso=0.2, valor=None)
+        T3 = Nota(peso=0.3, valor=None)
+        T4 = Nota(peso=0.3, valor=None)
         provas_que_quero = []
         trabalhos_que_quero = [T2, T3, T4]
         
-        
-        boletim = Boletim(provas_que_quero=provas_que_quero, provas_que_tenho=provas_que_tenho, trabalhos_que_quero=trabalhos_que_quero, trabalhos_que_tenho=trabalhos_que_tenho)
+        peso_prova = 1.0
+        peso_trabalho = T1.peso + T2.peso + T3.peso + T4.peso
+
+        boletim = Boletim(peso_prova=peso_prova, peso_trabalho=peso_trabalho, provas_que_quero=provas_que_quero, provas_que_tenho=provas_que_tenho, trabalhos_que_quero=trabalhos_que_quero, trabalhos_que_tenho=trabalhos_que_tenho)
         
         
         viewmodel = GradeOptmizerViewmodel(boletim).to_dict()
