0e59485ac3b715a847e00806ffbf229702fdd803
Vinícius de Oliveira Berti <vinicius.berti33@gmail.com>
2025-01-22 21:04:50 -0300

    🤑🤑🤑

--- tests/modules/calculate_mean/app/test_calculate_mean_usecase.py
+++ tests/modules/calculate_mean/app/test_calculate_mean_usecase.py
@@ -36,6 +36,36 @@ class TestCalculateMeanUsecase:
 
         with pytest.raises(InvalidInput):
             usecase(peso_prova=0.6, peso_trabalho = 0.4, provas_que_tenho=provas_que_tenho, trabalhos_que_tenho=trabalhos_que_tenho)
+
+    def test_calculate_mean_invalido_peso_trabalho(self):
+        provas_que_tenho = []
+        
+        T1 = Nota(peso=0.4*0.4, valor=9.5)
+        T2 = Nota(peso=0.4*0.6, valor=7)
+        trabalhos_que_tenho = [T1, T2]
+
+        peso_prova = 0.0
+        peso_trabalho = 0.7
+
+        usecase = CalculateMeanUsecase()
+
+        with pytest.raises(EntityParameterError):
+            usecase(peso_prova=peso_prova, peso_trabalho=peso_trabalho,provas_que_tenho=provas_que_tenho, trabalhos_que_tenho=trabalhos_que_tenho)
+
+    def test_calculate_mean_invalido_peso_prova(self):
+        P1 = Nota(peso=0.6*0.4, valor = 5)
+        P2 = Nota(peso=0.6*0.6, valor=4)
+        provas_que_tenho = [P1, P2]
+        
+        trabalhos_que_tenho = []
+
+        peso_prova = 0.7
+        peso_trabalho = 0.0
+
+        usecase = CalculateMeanUsecase()
+
+        with pytest.raises(EntityParameterError):
+            usecase(peso_prova=peso_prova, peso_trabalho=peso_trabalho,provas_que_tenho=provas_que_tenho, trabalhos_que_tenho=trabalhos_que_tenho)
             
     def test_calculate_mean_prova_without_value(self):
         P1 = Nota(peso=0.6*0.4)
