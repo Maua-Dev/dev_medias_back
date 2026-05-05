faa102d74c881edd9325cea66f0019f47d462e3e
veloy25 <veloy.devmaua@gmail.com>
2026-05-05 17:46:36 -0300

    fix: rename class notice to Notice and update severity import for consistency

--- src/shared/domain/entities/notice.py
+++ src/shared/domain/entities/notice.py
@@ -1,17 +1,19 @@
 from src.shared.helpers.errors.domain_errors import EntityError, EntityParameterError
 from pydantic import *
-from ..enums.severity_enum import severity
+from ..enums.severity_enum import SEVERITY
 
-class notice(BaseModel):
+class Notice(BaseModel):
     title: str = Field(
+        ...,
         description="Titulo do anúncio",
-        min_length=1,
         max_length=10
     )
     description: str = Field (
+        ...,
         description="Descrição do anúncio",
     )
-    severity: severity = Field(
+    severity: SEVERITY = Field(
+        ...,
         description="Gravidade do anúncio"
     )
     
