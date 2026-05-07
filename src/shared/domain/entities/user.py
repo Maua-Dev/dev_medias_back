5cc4cdcbdce9aed09121225032621c6dbeefdb58
veloy25 <veloy.devmaua@gmail.com>
2026-05-07 18:05:37 -0300

    fix(user): inherit from BaseModel in User class

--- src/shared/domain/entities/user.py
+++ src/shared/domain/entities/user.py
@@ -2,7 +2,7 @@ from pydantic import *
 from ..enums.role_enum import ROLE
 import uuid
 
-class User:
+class User(BaseModel):
     id:str= Field(
         default_factory=lambda: str(uuid.uuid4()),
         description="Id unico do usuário"
