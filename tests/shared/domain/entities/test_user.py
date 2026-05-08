49957ac91658950ff988049dda89b7833f21b6b3
veloy25 <veloy25.devmaua@gmail.com>
2026-05-08 16:14:29 -0300

    test(user): enhance User validation tests for name and role fields

--- tests/shared/domain/entities/test_user.py
+++ tests/shared/domain/entities/test_user.py
@@ -34,11 +34,58 @@ class Test_User:
         
     
     def test_id_missing(self):
-        pass
+        user = User(
+            name = "João",
+            role = ROLE.STUDENT    
+            )
+        
+        assert user.id != None
+        assert type(user.id) == str
     
     def test_name_not_str(self):
-        pass
-    
+        with pytest.raises(ValidationError) as err:
+            user = User(
+            id = str(uuid.uuid4()),
+            name = 2,
+            role = ROLE.STUDENT    
+            )
+        
+        error = err.value.errors()
+        
+        assert error[0]["loc"] == ("name",)
+        assert error[0]["type"] == "string_type"
+        
+    def test_name_missing(self):
+        with pytest.raises(ValidationError) as err:
+            user = User(
+            id = str(uuid.uuid4()),
+            role = ROLE.STUDENT    
+            )
+        
+        error = err.value.errors()
+        
+        assert error[0]["loc"] == ("name",)
+        assert error[0]["type"] == "missing"
+                
     def test_role_not_ROLE(self):
-        pass
+        with pytest.raises(ValidationError) as err:
+            user = User(
+            id = str(uuid.uuid4()),
+            name = "João",
+            role = "A"    
+            )
+        
+        error = err.value.errors()
+        
+        assert error[0]["loc"] == ("role",)
+        assert error[0]["type"] == "enum"
     
+    def test_role_missing(self):
+        user = User(
+            id = str(uuid.uuid4()),
+            name = "João"
+            )
+        
+        assert user.role != None
+        assert type(user.role) == ROLE
+        assert user.role == ROLE.STUDENT
