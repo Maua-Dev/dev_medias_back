73e184f576b1f7cf391f71e55f42810d6a44ea06
veloy25 <veloy.devmaua@gmail.com>
2026-05-25 16:43:29 -0300

    fix(notice): add an id atribute

--- src/shared/domain/entities/notice.py
+++ src/shared/domain/entities/notice.py
@@ -1,7 +1,12 @@
 from pydantic import BaseModel, Field
 from ..enums.severity_enum import SEVERITY
+import uuid
 
 class Notice(BaseModel):
+    id:str= Field(
+        default_factory=lambda: str(uuid.uuid4()),
+        description="Id unico do anúncio"
+    )
     title: str = Field(
         ...,
         description="Titulo do anúncio",
